53ef94ae5111868a770efb4805f9aec4899428a2
AhmetVARAN <102433124+AhmetVARAN@users.noreply.github.com>
2022-05-23 19:02:56 +0300

    Add files via upload

---
+++ src/main/java/cdi/_04_New.java
@@ -0,0 +1,28 @@
+package cdi;
+
+import com.ahmetvaran.StudentDto;
+import lombok.Getter;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.New;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named(value = "newtuto")
+@ApplicationScoped
+public class _04_New implements Serializable {
+
+    //üretilen
+    @Produces
+    public String uretilenData(@New StudentDto studentDto){
+        studentDto = StudentDto.builder().studentId(0L).studentName("studen adı").build();
+
+        return studentDto.getStudentName();
+    }
+
+    @Getter
+    @Inject
+    private String tuketilenData;
+}
