e7cb789a987aa9fe01bb77cd0a44709aef02490f
AhmetVARAN <ahmetvaran2015@gmail.com>
2022-05-23 19:00:50 +0300

    Cdi New

---
+++ src/main/java/com/ahmetvaran/StudentDto.java
@@ -0,0 +1,20 @@
+package com.ahmetvaran;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//DataTransferObject = DTO
+public class StudentDto {
+
+    private Long studentId;
+    private String studentName;
+    private String studentSurname;
+}
